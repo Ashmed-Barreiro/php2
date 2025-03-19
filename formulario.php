eb61af001f82063da27c5e27eb6324d7d6513d01
Ashmed Barreiro <ashmed.barreiro.7e8@itb.cat>
2025-03-19 08:45:00 +0000

    Formulario

--- formulario.php
+++ formulario.php
@@ -6,7 +6,7 @@
     <div class="container">
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (!empty($_POST['nom'] or $_POST['Apellido'] or $_POST['Genero'])or $_POST['Email']or $_POST['Edad']or $_POST['Ciudad']or $_POST['aficiones']or $_POST['motivaciones']) {
+            if (!empty($_POST['nom'] or $_POST['Apellidos'] or $_POST['Genero'])or $_POST['Email']or $_POST['Edad']or $_POST['Ciudad']or $_POST['aficiones']or $_POST['motivaciones']) {
                 $nom = htmlspecialchars(trim($_POST['nom']));
                 $apellido = htmlspecialchars(trim($_POST['Apellido']));
                 $genero = htmlspecialchars(trim($_POST['Genero']));
@@ -15,9 +15,9 @@
                 $ciudad = htmlspecialchars(trim($_POST['Ciudad']));
                 $aficiones = htmlspecialchars(trim($_POST['aficiones']));
                 $motivaciones = htmlspecialchars(trim($_POST['motivaciones']));
-                echo "<h2>Hola, $nom $apellido, de genero: $genero, con email: $email, con $edad años, que vive en $ciudad, que le gusta $aficiones, y que lo motiva $motivaiones </h2>";
+                echo "<h2>Hola, $nom con apellido $apellido, de genero: $genero, con email: $email, con $edad años, que vive en $ciudad, que le gusta $aficiones, y que lo motiva $motivaciones </h2>";
             } else {
-                echo "<h2>El camp $ està buit. Si us plau, introdueix el teu nom.</h2>";
+                echo "<h2>Els camps estàn buits. Si us plau, introdueix dades2<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<.</h2>";
             }
             
         } else {
